c5c7edbb16b8c2a618774fa3841537e59a3cda7d
Richa Meherwal <meherwa2@illinois.edu>
2022-08-14 17:09:58 -0700

    Adding scripts to merge slides into a lecture pdf with the lecture name

---
+++ scripts/merger_pdf.py
@@ -0,0 +1,30 @@
+from PyPDF2 import PdfFileMerger
+import re
+def sort_slide_names(l): 
+    """ Sort the given iterable in the way that humans expect.""" 
+    convert = lambda text: int(text) if text.isdigit() else text 
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key) ] 
+    sl = sorted(l, key = alphanum_key)
+    try:
+        sl.remove('.DS_Store')
+    except:
+        pass
+    return sl 
+
+import os
+
+path = "/Users/richameherwal/Desktop/UIUC/research/eduweb/EducationalWeb/pdf.js/static/slides/CS410"
+dirs = os.listdir( path )
+dirs.remove('.DS_Store')
+
+for lecture in dirs:
+    lec_folder= os.listdir(os.path.join(path, lecture))
+    sorted_file_list = sort_slide_names(lec_folder)
+#     print(sorted_file_list)
+    merger = PdfFileMerger()
+    for slide in sorted_file_list:
+        pdf_file_path=os.path.join(path,lecture,slide)
+#         print(pdf_file_path)
+        merger.append(pdf_file_path)
+    merger.write(path+'/'+lecture+'.pdf')
+    merger.close()
