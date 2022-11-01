0db743791652773bcf4b3d3a1c3d633ef90391b5
Richa Meherwal <meherwa2@illinois.edu>
2022-10-31 19:25:06 -0700

    Updated filter function

--- static/filter_courses.js
+++ static/filter_courses.js
@@ -1,19 +1,16 @@
-// used for search bar in courses' and lectures' dropdown menu
-function dropdownSearchbarFilterFunction(inputID, dropdownMenuID) {
-  // var input, filter, ul, li, courseList, i;
-  var input = document.getElementById(inputID); // finds the input according to the input's id
-  var inputText = input.value.toUpperCase(); // takes in user's input string and puts it all in upper case
-  var dropdownList = document.getElementById(dropdownMenuID); // gets the dropdown menu
-  var links = dropdownList.getElementsByTagName("a"); // gets all the links to courses/lectures (which each have <a> tags)
-  var name; // name of lecture/course
-    for (i = 0; i < links.length; i++) {
-    name = links[i].textContent; // gets the actual name of the course or lecture, including spaces
-    // indexOf method returns -1 if the value is not found
-    if (name.toUpperCase().indexOf(inputText) > -1) {
-      links[i].style.display = "";
-    } else {
-      // no course name corresponds to user input so we delete course name from dropdown (i.e, "none")
-      links[i].style.display = "none";
+// @param: inputID is id of the user's text input; dropdownMenuID is id of relevant dropdown menu (e.g, lecture, courses, etc)
+// Filters dropdown menu items according to matches with user input. Note that spacing does not matter.
+ function dropdownSearchbarFilterFunction(inputID, dropdownMenuID) {
+      var input = document.getElementById(inputID);
+      var inputText = input.value.toUpperCase().replace(/\s/g, '');
+      var dropdownList = document.getElementById(dropdownMenuID);
+      var links = dropdownList.getElementsByTagName("a");
+      for (i = 0; i < links.length; i++) {
+        var name = links[i].innerText.replace(/\s/g, '');
+        if (name.toUpperCase().indexOf(inputText) > -1) {
+          links[i].style.display = "";
+        } else {
+          links[i].style.display = "none";
+        }
+      }
     }
-  }
-}
